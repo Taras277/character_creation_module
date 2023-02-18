0bf87e22fc8b330ae32cfb79869fa165038e462e
Тарас Когут <kogut777@yandex.ru>
2023-02-18 19:18:15 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def attack(char_name: str, char_class: str) -> str:
     if char_class == 'healer':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(-3, -1)}')
+                
 
 
 def defence(char_name: str, char_class: str) -> str:
